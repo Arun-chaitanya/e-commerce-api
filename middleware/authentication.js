653c43e210a936bb0fbefe583369b5deaf003651
Arun-chaitanya <arunchaitanya.iitkgp@gmail.com>
2023-07-12 22:27:52 +0530

    admin permissions and unauthorize error setup

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,3 +1,25 @@
-const authMiddleware = async (req, res, next) => {
+const { UnauthenticatedError, UnauthorizedError } = require("../errors");
+const { isTokenValid } = require("../utils");
+
+const authenticateUser = async (req, res, next) => {
+  const token = req.signedCookies.token;
+  if (!token) {
+    throw new UnauthenticatedError("Authentication Invalid");
+  }
+  try {
+    const { name, role, userId } = isTokenValid({ token });
+    req.user = { name, role, userId };
+    next();
+  } catch (e) {
+    throw new UnauthenticatedError("Authentication Invalid");
+  }
+};
+
+const authorizePermissions = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    throw new UnauthorizedError("Unauthorized to access this route!");
+  }
   next();
 };
+
+module.exports = { authenticateUser, authorizePermissions };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,13 +7,17 @@ const {
   updateUser,
   updateUserPassword,
 } = require("../controllers/userController");
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require("../middleware/authentication");
 
-router.route("/").get(getAllUsers);
+router.route("/").get(authenticateUser, authorizePermissions, getAllUsers);
 
 router.route("/showMe").get(showCurrentUser);
 router.route("/updateUser").patch(updateUser);
 router.route("/updateUserPassword").patch(updateUserPassword);
 
-router.route("/:id").get(getSingleUser);
+router.route("/:id").get(authenticateUser, authorizePermissions, getSingleUser);
 
 module.exports = router;
