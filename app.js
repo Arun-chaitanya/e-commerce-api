1123e53e971910df9a072155d84bff9805d31383
Arun-chaitanya <arunchaitanya.iitkgp@gmail.com>
2023-07-14 22:49:48 +0530

    product controller setup

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const connectDB = require("./db/connect");
 //importing routes
 const authRouter = require("./routes/authRouter");
 const userRouter = require("./routes/userRouter");
+const productRouter = require("./routes/productRouter");
 
 app.use(morgan("tiny"));
 app.use(express.json());
@@ -22,6 +23,7 @@ app.use(cookieParser(process.env.JWT_SECRET));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
 
 app.use("/", (req, res) => {
   res.send("E-Commerce API");
